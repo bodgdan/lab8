a3aa60143a148cfcd9397075b4671b30cc7dfb6a
bodgdan <bodgj61@gmail.com>
2023-12-12 16:27:52 +0200

    first commit

---
+++ script8.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+apt update -y 
+apt install apache2 -y
+systemctl start apache2
+systemctl enable apache2
+
+
+cat <<EOF > ssl-config.conf
+[ req ]
+default_bits       = 2048
+default_md         = sha256
+prompt             = no
+encrypt_key        = no
+distinguished_name = dn
+
+[ dn ]
+C=US
+ST=New York
+L=New York City
+O=My Organization
+OU=My Organizational Unit
+emailAddress=me@example.com
+CN = www.mywebsite.com
+EOF
+
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -config ssl-config.conf
+
+
+cat <<EOF > apache-config.conf
+<VirtualHost *:443>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/html
+    SSLEngine on
+    SSLCertificateFile /etc/ssl/certs/apache-selfsigned.crt
+    SSLCertificateKeyFile /etc/ssl/private/apache-selfsigned.key
+</VirtualHost>
+EOF
+
+
+sudo cp apache-config.conf /etc/apache2/sites-available/000-default.conf
+sudo a2enmod ssl
+sudo a2ensite default-ssl
+sudo systemctl restart apache2
+
+
+echo "<html><body><h1>Hello world! Hovda Bohdan done it</h1></body></html>" | sudo tee /var/www/html/index.html
